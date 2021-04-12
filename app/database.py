9bc73c79d0a9fb075e27d6c1b23f849bf50c7c95
Ground Zhou <groundzhou@outlook.com>
2021-04-12 16:48:39 +0800

    后端项目搭建

---
+++ app/database.py
@@ -0,0 +1,58 @@
+import click
+import psycopg2
+from flask.cli import with_appcontext
+from flask import g, current_app
+
+
+def get_db():
+    """
+    获取数据库连接
+    """
+    if 'db' not in g:
+        g.db = psycopg2.connect(
+            dbname=current_app.config['DB_NAME'],
+            user=current_app.config['DB_USER'],
+            password=current_app.config['DB_PASSWORD'],
+            host=current_app.config['DB_HOST'],
+            port=current_app.config['DB_PORT']
+        )
+
+    return g.db
+
+
+def close_db(e=None):
+    """
+    关闭数据库
+    """
+    db = g.pop('db', None)
+
+    if db:
+        db.close()
+
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    db = get_db()
+    with db.cursor() as cur:
+        cur.execute('CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);')
+    db.commit()
+    click.echo('Initialized the database.')
+
+
+@click.command('drop-db')
+@with_appcontext
+def drop_db_command():
+    """Clear all tables."""
+    db = get_db()
+    with db.cursor() as cur:
+        cur.execute('DROP TABLE test;')
+    db.commit()
+    click.echo('Drop all tables.')
+
+
+def init_app(app):
+    app.cli.add_command(init_db_command)
+    app.cli.add_command(drop_db_command)
+    app.teardown_appcontext(close_db)
